d48c89b9b1548be25897f08f67eeb032dd7113d5
vamsibabu <vamsiguddanti3@gmail.com>
2025-11-04 20:01:09 +0530

    class task coompleted

---
+++ portifiliovamsi/jstasks/classtask/javascript.js
@@ -0,0 +1,22 @@
+
+let count = 0;
+   function updateDisplay() {
+      document.getElementById('count').textContent = count;
+    }
+
+function increment() {
+  count++;
+updateDisplay()
+}
+
+
+
+function decrement() {
+  count--;
+  updateDisplay();
+}
+function reset(){
+    count =0;
+    updateDisplay();
+}
+
